002ae58fdf14cf959f5b6acce8ea4c78456f3f24
segni <segniwoldemichael@gmail.com>
2023-11-14 04:02:29 -0800

    Update task 1: Handling the d and i conversion specifiers

--- format_int.c
+++ format_int.c
@@ -25,14 +25,12 @@ int int_to_str(int num)
 			write(1, "-", 1);
 			num = -num;
 		}
-
 		while (num > 0)
 		{
 			digit = num % 10;
 			*ptr++ = digit + '0';
 			num /= 10;
 		}
-		
 		start = buffer;
 		end = ptr - 1;
 		while (start < end)
@@ -42,23 +40,20 @@ int int_to_str(int num)
 			*end-- = temp;
 		}
 	}
-
 	*ptr = '\0';
-
 	length = ptr - buffer;
 	write(1, buffer, length);
-
 	return (length);
 }
 
 /**
  * _printf - It is used formatted output.
- * 		In this case It can only handle the d and i conversion specifiers.
+ * In this case It can only handle the d and i conversion specifiers.
  * @format: It is format string argument.
  * @...: It is number of arguments.
  *
  * Return: The number of characters printed
- *		(excluding the null byte used to end output to strings)
+ * (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
@@ -70,20 +65,14 @@ int _printf(const char *format, ...)
 	num_of_char = 0;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
-
 	va_start(list_of_args, format);
-
 	while (*format)
 	{
 		if (*format != '%')
 		{
 			if (*format == '\n')
-			{
 				num_of_char++;
-			}
 			write(1, format, 1);
 			num_of_char++;
 		}
@@ -91,26 +80,20 @@ int _printf(const char *format, ...)
 		{
 			format++;
 			if (*format == '\0')
-			{
 				return (-1);
-			}
-
 			if (*format == 'd')
 			{
 				d = va_arg(list_of_args, signed int);
 				num_of_char += int_to_str(d);
 			}
-			
 			if (*format == 'i')
 			{
 				i = va_arg(list_of_args, signed int);
 				num_of_char += int_to_str(i);
 			}
 		}
-
 		format++;
 	}
-
 	va_end(list_of_args);
 	return (num_of_char);
 }
